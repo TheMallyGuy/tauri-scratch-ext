8fc892ccd611bea1a1aa8b1a6f76860070dbc10c
Mally Dang <no-reply@mally.qzz.io>
2026-09-07 13:06:24 +0700

    format must be es

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
       input: 'src/withL10n.ts',
       output: {
             file: 'output/extension.js',
-            format: 'esm',
+            format: 'es',
             // https://github.com/FurryR/scratch-ext/blob/main/tsup.config.ts
             banner:
                   '// Generated using Rolldown.js by VOID(0). (https://rolldown.rs/)\n' +
