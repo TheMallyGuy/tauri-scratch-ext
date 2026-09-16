d53d247383705e11cf3d20e8a251fa3df0c65bb1
Mally Dang <no-reply@mally.qzz.io>
2026-09-16 12:56:10 +0700

    i guess

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
             format: 'es',
             // https://github.com/FurryR/scratch-ext/blob/main/tsup.config.ts
             banner:
-                  '// Generated using Rolldown.js by VOID(0). (https://rolldown.rs/)\n' +
+                  '// Generated using Rolldown.rs by VOID(0). (https://rolldown.rs/)\n' +
                   '// If you want to see the source code, please view: https://github.com/TheMallyGuy/tauri-scratch-ext \n' +
                   '// Name: Tauri Extension\n' +
                   '// ID: tauriExtension\n' +
