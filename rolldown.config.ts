06a20de35d5029456cbc6f6ed00edccd6fffd8bf
Mally Dang <no-reply@mally.qzz.io>
2026-09-16 20:59:32 +0700

    chore: this just feel nicer

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
             format: 'es',
             // https://github.com/FurryR/scratch-ext/blob/main/tsup.config.ts
             banner:
-                  '// Generated using Rolldown.rs by VOID(0). (https://rolldown.rs/)\n' +
                   '// If you want to see the source code, please view: https://github.com/TheMallyGuy/tauri-scratch-ext \n' +
+                  '// Generated using Rolldown.rs by VOID(0). (https://rolldown.rs/)\n' +
                   '// Name: Tauri Extension\n' +
                   '// ID: tauriExtension\n' +
                   '// Description: Scratch extension for Tauri.\n' +
