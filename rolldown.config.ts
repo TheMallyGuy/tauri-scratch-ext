7125fc7bf7e32a7f563a02beb19c820c6827d228
Mally Dang <no-reply@mally.qzz.io>
2026-09-17 11:29:58 +0700

    configure rolldown for a smaller output

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,27 +1,28 @@
 import { defineConfig } from 'rolldown'
 
+const banner = '// If you want to see the source code, please view: https://github.com/TheMallyGuy/tauri-scratch-ext\n' +
+      '// Do not modify this extension for malicious uses.\n' +
+      '// Generated using Rolldown.rs by VOID(0). (https://rolldown.rs/)\n' +
+      '// Name: Tauri Extension\n' +
+      '// ID: tauriExtension\n' +
+      '// Description: Scratch extension for Tauri.\n' +
+      '// By: Mally\n' +
+      '// License: MPL-2.0\n'
+
 export default defineConfig({
       input: 'src/withL10n.ts',
-      output: {
-            comments: false,
+      treeshake: true,
+      output: [{
+            file: 'output/extensionSmall.js',
+            format: 'es',
+            minify: true,
+            sourcemap: false,
+            externalLiveBindings: false,
+            postBanner: banner
+      }, {
             file: 'output/extension.js',
             format: 'es',
-            // https://github.com/FurryR/scratch-ext/blob/main/tsup.config.ts
-            banner:
-                  '// If you want to see the source code, please view: https://github.com/TheMallyGuy/tauri-scratch-ext \n' +
-                  '// Do not modify this extension for malicious uses.\n' +
-                  '// Generated using Rolldown.rs by VOID(0). (https://rolldown.rs/)\n' +
-                  '// Name: Tauri Extension\n' +
-                  '// ID: tauriExtension\n' +
-                  '// Description: Scratch extension for Tauri.\n' +
-                  '// By: Mally\n' +
-                  '// License: MPL-2.0\n' +
-                  '//\n' +
-                  '//\n' +
-                  '//\n' +
-                  '//\n' +
-                  '//\n' +
-                  '//\n' +
-                  '//\n'
-      }
+            sourcemap: false,
+            postBanner: banner
+      }],
 })
